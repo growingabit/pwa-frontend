c6787e8e48c914db8646c8750d32758b4b5b4398
Alessandro Aglietti <alessandro.aglietti@gmail.com>
2017-09-30 01:51:05 +0200

    dajee

---
+++ dist/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+gsutil -m rm -R gs://abc-growbit-xyz-backend/
+
+gsutil -m cp -R . gs://abc-growbit-xyz-backend/
+
+gsutil acl ch -r -u AllUsers:R gs://abc-growbit-xyz-backend
+
+gcloud compute url-maps invalidate-cdn-cache growingabit-io --path "/*"
