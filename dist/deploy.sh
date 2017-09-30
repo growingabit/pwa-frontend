46f00fc32537c43c3710da4a02c5affe985cfbd4
Alessandro Aglietti <alessandro.aglietti@gmail.com>
2017-09-30 02:17:37 +0200

    spa on gcs rulez

--- dist/deploy.sh
+++ dist/deploy.sh
@@ -9,4 +9,6 @@ gsutil rm gs://abc-growbit-xyz-backend/deploy.sh
 
 gsutil acl ch -r -u AllUsers:R gs://abc-growbit-xyz-backend
 
+gsutil web set -m index.html -e index.html gs://abc-growbit-xyz-backend
+
 gcloud compute url-maps invalidate-cdn-cache growingabit-io --path "/*"
